b5b34ec86d23c8e3541442da3eed09f78ca995f0
Amal Vijay <avijay@isimtl.local>
2019-11-07 15:53:37 -0500

    commit file

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+//myvar = 0;
+$myvar = 0;var_dump($myvar);
+$var5 = 0;var_dump($var5);
+$_myvar = 0;var_dump($_myvar);
+$_5var = 0;var_dump($_5var);
+$__element1 = 0;var_dump($__element1);
+//$age* = 0;var_dump($age);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Variables identifiers</title>
+</head>
+<body>
+    <header>
+        <h1>Variables identifiers</h1>
+    </header>
+    <main>
+    </main>
+</body>
+</html>
